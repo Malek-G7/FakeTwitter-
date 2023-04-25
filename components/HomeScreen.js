b951fff591f12c8430b879894878edeb34833c28
Elaine <g00370088@gmit.ie>
2023-04-25 09:21:04 +0100

    Can't merge

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -22,6 +22,49 @@ const HomeScreen = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={styles.outer}>
       <View style={styles.container}>
+        <View style={styles.add}>
+          <Text style={styles.text}>Add a post!</Text>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Give it a title!"
+            onChangeText={(newName) => {
+              setNewProduct({ ...newProduct, name: newName });
+            }}
+          />
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Start typing!"
+            onChangeText={(newPrice) => {
+              setNewProduct({ ...newProduct, price: newPrice });
+            }}
+          />
+          <Pressable
+            style={styles.button}
+            onPress={async () => {
+              let data;
+              try {
+                const res = await fetch(`${uri}/addProduct`, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "ngrok-skip-browser-warning": "69420", // See: https://stackoverflow.com/questions/73017353/how-to-bypass-ngrok-browser-warning
+                  },
+                  body: JSON.stringify({
+                    productName: newProduct.name,
+                    productPrice: newProduct.price,
+                  }), // Need to use POST to send body
+                });
+                data = await res.json();
+              } catch (err) {
+                console.log(err);
+              }
+              // navigation.navigate('product', {product : data })
+            }}
+          >
+            <Text style={styles.Text}>Add post</Text>
+          </Pressable>
+        </View>
+        {/* //============================================================================================================================================================ */}
         <View style={styles.proPage}>
           <Text style={styles.text}>Browse posts</Text>
           <Pressable
